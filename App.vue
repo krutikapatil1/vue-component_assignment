f12e0a06c31c78b8d090d1755b38348d60887023
krutikapatil1 <krutiks89@gmail.com>
2020-04-13 23:06:15 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <div class="row">
+      <app-servers></app-servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import ServerDetails from "./ServerDetails.vue";
+import Servers from "./Servers.vue";
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-server-details": ServerDetails,
+    "app-servers": Servers
+  }
+};
+</script>
+
+<style>
+</style>
